eb680988621f350203ce0e6a9502eba4e91cea73
sandipsky <sandipshakya75@gmail.com>
2024-06-18 11:37:49 +0545

    changes

--- angular/directives/sortable/sortable-header.directive.ts
+++ angular/directives/sortable/sortable-header.directive.ts
@@ -1,84 +1,29 @@
-// import { Directive, EventEmitter, Input, Output } from '@angular/core';
-
-// export type SortColumn = keyof  any | '';
-// export type SortDirection = 'asc' | 'desc' | '';
-
-// const rotate: {[key: string]: SortDirection} = {
-//   asc: 'desc',
-//   desc: '',
-//   '' : 'asc',
-// };
-
-// export const compare = (
-//   v1: string | number | boolean | Date,
-//   v2: string | number | boolean | Date,
-// ) => (v1 < v2 ? -1 : v1 > v2 ? 1 : 0);
-
-// export interface SortEvent {
-//   column : SortColumn;
-//   direction: SortDirection;
-// }
-
-// @Directive({
-//   selector: 'th[sortable]',
-//   host: {
-//     '[class.asc]': 'direction === "asc"',
-//     '[class.desc]': 'direction === "desc"',
-//     '(click)': 'rotate()',
-//   },
-// })
-// export class SortableHeaderDirective {
-
-//   constructor() {
-//   }
-
-//   @Input() sortable: SortColumn = '';
-//   @Input() direction: SortDirection = '';
-//   @Output() sort = new EventEmitter<SortEvent>();
-
-//   public rotate() : void {
-//     this.direction = rotate[this.direction];
-//     this.sort.emit({
-//       column: this.sortable,
-//       direction: this.direction
-//     });
-//   };
-
-// }
-
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
 
-export type SortColumn = keyof  any | '';
-export type SortDirection = 'asc' | 'desc' | '';
-
-export const compare = (
-  v1: string | number | boolean | Date,
-  v2: string | number | boolean | Date,
-) => (v1 < v2 ? -1 : v1 > v2 ? 1 : 0);
-
 export interface SortEvent {
-  column : SortColumn;
-  direction: SortDirection;
+  column: string;
+  direction: 'asc' | 'desc' | '';
 }
 
 @Directive({
-  selector: 'th[sortable]',
+  selector: 'th[sortValue]',
+  standalone: true
 })
-export class SortableHeaderDirective {
-  @Input() sortable: SortColumn = '';
-  @Input() direction: SortDirection = '';
-  @Output() sort = new EventEmitter<SortEvent>();
-  static prevSortable: SortColumn = '';
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+export class SortableHeaderDirective {
+  @Input() sortable: string = '';
+  @Input() direction: 'asc' | 'desc' | '' = '';
+  @Output() sort = new EventEmitter<SortEvent>();
+  static prevSortable: string = '';
+
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   @HostListener('click') onClick() {
-    if(SortableHeaderDirective.prevSortable == this.sortable)
-    {
+    console.log('asdf')
+    if (SortableHeaderDirective.prevSortable == this.sortable) {
       this.toggleSortDirection();
     }
-    else 
-    {
+    else {
       SortableHeaderDirective.prevSortable = this.sortable;
       this.direction = 'asc';
     }
@@ -89,7 +34,7 @@ export class SortableHeaderDirective {
   updateStatus() {
 
     const siblings = this.el.nativeElement.parentElement.querySelectorAll('th[sortable]');
-    siblings.forEach((sibling:any) => {
+    siblings.forEach((sibling: any) => {
       if (sibling !== this.el.nativeElement) {
         const caretElement = sibling.querySelector('a');
         if (caretElement) {
@@ -108,7 +53,7 @@ export class SortableHeaderDirective {
       });
 
       const directionClass = this.direction === 'asc' ? 'asc' :
-                             this.direction === 'desc' ? 'desc' : 'none';
+        this.direction === 'desc' ? 'desc' : 'none';
       this.renderer.addClass(caretElement, directionClass);
     }
   }
