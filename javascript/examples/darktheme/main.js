0171d9ed4f77bbf989906874159c30d1f9cc59ad
sandipsky <sandipshakya75@gmail.com>
2025-12-26 17:53:28 +0545

    changes

--- javascript/examples/darktheme/main.js
+++
@@ -1,8 +0,0 @@
-// Select the toggle button
-const themeToggle = document.getElementById('themeToggle');
-
-// Add a click event listener
-themeToggle.addEventListener('click', () => {
-  // Toggle the 'dark' class on the body
-  document.body.classList.toggle('dark');
-});
